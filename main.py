1f361f9cb51004dd540f1db350e9db6c4a19b325
Veltrixcoder <veltrix620@gmail.com>
2026-02-22 09:56:54 +0530

    Add CORS middleware to FastAPI app
    Added CORS middleware to the FastAPI application to allow cross-origin requests.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import subprocess
 import threading
 import re
@@ -8,6 +9,15 @@ import os
 import socket
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 HF_URLS = [
     "https://cursorpro-streamion.hf.space",
